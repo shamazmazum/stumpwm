2729d0f76f8192d364b1f83724132bde54257df7
naryl <cy@ngs.ru>
2012-10-25 21:30:36 +0400

    Export a useful function from menu.lisp

--- menu.lisp
+++ menu.lisp
@@ -26,7 +26,8 @@
 
 (in-package #:stumpwm)
 
-(export '(select-from-menu))
+(export '(select-from-menu
+          menu-filter-regexp))
 
 (defvar *menu-map* nil
   "The keymap used by the interactive menu.")
