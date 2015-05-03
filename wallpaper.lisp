bc2c3682afc0e0c1917f429739097b920a3fb705
Vasily Postnicov <shamaz.mazum@gmail.com>
2015-05-03 11:37:45 +0300

    A little more work on wallpapers: 	1. You can now remove a wallpaper from screen with #'stumpwm:clear-wallpaper            or 'wallpaper' command and an empty string as its argument. 	2. You can select a wallpaper which is less in size than the screen. It will 	   be shown centered.
    	3. Rework _XSETROOT_ID property handling. StumpWM will not set this property by
    	   itself, but rather delete this property from the root screen and kill client
    	   which owns the pixmap in this property (also freeing its resources) when
    	   a new wallpaper is set. This is good with other wallpaper setters like xli.

--- wallpaper.lisp
+++ wallpaper.lisp
@@ -2,34 +2,69 @@
 
 (export '(*image-loaders*
           load-wallpaper
+          clear-wallpaper
           supported-wallpaper-types))
 
 (defparameter *image-loaders* nil
   "Functions which load image files into Ximage")
 
-(defun free-wallpaper (screen)
-  (let* ((root-window (screen-root screen))
-         (old-pixmap (xlib:get-property root-window :_XSETROOT_ID
-                                        :type :PIXMAP :transform
-                                        (lambda (id) (xlib::lookup-pixmap (xlib:window-display root-window) id)))))
-    (if old-pixmap (xlib:free-pixmap (first old-pixmap)))))
+(defun set-wallpaper-property (screen pixmap)
+  (xlib:change-property (screen-root screen) :_XSETROOT_ID (list pixmap) :PIXMAP 32
+                        :transform #'xlib:pixmap-id))
+
+(defun get-wallpaper-property (screen)
+  (let ((root-window (screen-root screen)))
+    (xlib:get-property root-window :_XSETROOT_ID
+                       :type :PIXMAP :transform
+                       (lambda (id) (xlib::lookup-pixmap (xlib:window-display root-window) id)))))
+
+(defun free-non-native-wallpaper (screen)
+  "Free the wallpaper set by an external program which wished
+   to retain its resources (like xli)"
+  (let ((old-pixmaps (get-wallpaper-property screen)))
+    (when old-pixmaps
+      (xlib:kill-client *display* (xlib:pixmap-id (car old-pixmaps)))
+      #+nil (xlib:free-pixmap old-pixmap)
+      (xlib:delete-property (screen-root screen) :_XSETROOT_ID))))
 
 (defun set-wallpaper (screen image)
-  (if (not
-       (and (= (screen-width screen) (xlib:image-width image))
-            (= (screen-height screen) (xlib:image-height image))
-            (= (screen-depth screen) (xlib:image-depth image))))
-      (throw 'error "Bad Match"))
+  (let ((image-width (xlib:image-width image))
+        (image-height (xlib:image-height image))
+        (image-depth (xlib:image-depth image))
+        (screen-width (screen-width screen))
+        (screen-height (screen-height screen))
+        (screen-depth (screen-depth screen)))
+    (if (or
+         (> image-width screen-width)
+         (> image-height screen-height))
+        (throw 'error "Image is too big"))
+    (if (/= image-depth screen-depth)
+        (throw 'error "Unsupported color depth"))
 
-  (free-wallpaper screen)
+  (free-non-native-wallpaper screen)
   (let* ((root-window (screen-root screen))
-         (pixmap (xlib:image-pixmap root-window image)))
+         (pixmap (xlib:create-pixmap :width screen-width
+                                     :height screen-height
+                                     :depth screen-depth
+                                     :drawable root-window))
+         (gc (xlib:create-gcontext :drawable pixmap
+                                   :foreground (screen-bg-color screen))))
 
+    ;; Kludge. Fill pixmap with background color if sizes do not match
+    (if (or (/= screen-width image-width)
+            (/= screen-height image-height))
+        (xlib:draw-rectangle pixmap gc 0 0 screen-width screen-height t))
+    (xlib:put-image pixmap gc image
+                    :x (floor (- screen-width image-width) 2)
+                    :y (floor (- screen-height image-height) 2))
+    (xlib:free-gcontext gc)
     (setf (xlib:window-background root-window) pixmap)
     ;; Update _XSETROOT_ID property so we can free the pixmap later
-    (xlib:change-property root-window :_XSETROOT_ID (list pixmap) :PIXMAP 32
-                          :transform #'xlib:pixmap-id)
-    (xlib:clear-area root-window)))
+    ;; FIXME: Do not use it, as we can shoot ourselves later
+    #+nil (set-wallpaper-property screen pixmap)
+    ;; Rather free it now, as it is not used anywhere else currently
+    #+t (xlib:free-pixmap pixmap)
+    (xlib:clear-area root-window))))
 
 (defun load-wallpaper (screen name)
   "Load a wallpaper with the NAME onto the screen"
@@ -45,11 +80,20 @@
           (set-wallpaper screen (funcall (cdr loader) pathname))
           (throw 'error "Image format not understood")))))
 
+(defun clear-wallpaper (screen)
+  (free-non-native-wallpaper screen)
+  (let ((root-window (screen-root screen)))
+    (setf (xlib:window-background root-window) :parent-relative)
+    (xlib:clear-area root-window)))
+
 (defun supported-wallpaper-types ()
   "Return supported wallpaper types"
   (mapcar #'car *image-loaders*))
 
 (defcommand wallpaper (filename)
     ((:string "File name: "))
-  "Load a wallpaper on the current screen"
-  (load-wallpaper (current-screen) filename))
+  "Load a wallpaper on the current screen
+   or remove the old one if a new name is not given"
+  (if (string/= filename "")
+      (load-wallpaper (current-screen) filename)
+      (clear-wallpaper (current-screen))))
