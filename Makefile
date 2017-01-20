e9c7669572ae64be94a1f609398bb7b20446501b
Vasily Postnicov <shamaz.mazum@gmail.com>
2017-01-20 20:04:11 +0300

    New type of StumpWM builds: build with preloaded (built-in) modules.
    You can specify modules you want to be preloaded in stumpwm in
    builtin-modules.lisp-expr file in this directory which contains a list of
    module names, in example, ("foo" "bar" "cool-module"). These modules will be
    initialized when you launch stumpwm, no call to load-module is required in your
    .stumpwmrc file.
    
    Bug: Some modules (like wallpaper module) can not be built-in in this way,
    because they require late initialization and built-in modules all are
    initialized very early on StumpWM startup.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 stumpwm: *lisp
-	env STUMPWM_TARGET=${.TARGET} sbcl --load build.lisp
+	@echo "You can build StumpWM with built-in modules,"
+	@echo "just specify them in builtin-modules.lisp-expr file as a list of strings"
+	env STUMPWM_TARGET="${.TARGET}" sbcl --load build.lisp
 
 clean:
 	rm -f stumpwm
--- build.lisp
+++ build.lisp
@@ -2,10 +2,22 @@
 
 (require 'sb-posix)
 
+(defun get-builtin-modules ()
+  (let ((*read-eval* nil))
+    (handler-case
+        (with-open-file (in "builtin-modules.lisp-expr")
+          (read in))
+      (file-error () ()))))
+
 (defun get-target-name ()
   (sb-posix:getenv "STUMPWM_TARGET"))
 
 (ql:quickload :stumpwm)
+
+(let ((builtin-modules (get-builtin-modules)))
+  (when builtin-modules
+    (format t "Building with modules ~{~a~^, ~}~%" builtin-modules)
+    (mapc #'stumpwm:load-module builtin-modules)))
 
 (sb-ext:save-lisp-and-die (get-target-name)
                           :executable t
